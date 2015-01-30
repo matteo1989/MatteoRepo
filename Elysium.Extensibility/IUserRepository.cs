2a7e1766d6b181112f259259a2e654734c786756
matteo1989 <kissmatearpad@outlook.com>
2015-01-30 23:19:17 +0100

    Basement

---
+++ Elysium.Extensibility/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium.Extensibility
+{
+    public interface IUserRepository
+    {
+        void AddUser();
+
+        void UpdateUser();
+
+        void DeleteUser();
+    }
+}
