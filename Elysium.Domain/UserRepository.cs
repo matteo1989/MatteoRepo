bdf2bbdbc7c880addd6c6aab760791547cd4d3ed
matteo1989 <kissmatearpad@outlook.com>
2015-01-30 23:36:35 +0100

    Add entities 1

---
+++ Elysium.Domain/UserRepository.cs
@@ -0,0 +1,27 @@
+﻿using Elysium.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium.Domain
+{
+    public class UserRepository : IUserRepository
+    {
+        public void AddUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUser()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
