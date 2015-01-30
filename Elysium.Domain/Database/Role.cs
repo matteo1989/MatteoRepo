2931b4d5d4e9d96b94687ff89b5e304d7773a0a6
matteo1989 <kissmatearpad@outlook.com>
2015-01-30 23:50:02 +0100

    addd

---
+++ Elysium.Domain/Database/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium.Domain.Database
+{
+    public enum Role
+    {
+        Administrator,
+        Officer,
+        Librarian,
+        Revisor,
+        Knight
+    }
+}
