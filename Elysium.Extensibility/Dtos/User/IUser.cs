3b5281ea6619d541c0c853c9dc9d930eb54ee506
matteo1989 <kissmatearpad@outlook.com>
2015-01-30 23:27:25 +0100

    Adduser

---
+++ Elysium.Extensibility/Dtos/User/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elysium.Extensibility.Dtos.User
+{
+    public interface IUser
+    {
+       int Id { get; set; }
+
+       string UserName { get; set; }
+
+       string FirstName { get; set; }
+
+       string LastName { get; set; }
+    }
+}
