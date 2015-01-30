d342758c3d52564bcf304af9da51220a9400147e
matteo1989 <kissmatearpad@outlook.com>
2015-01-30 23:52:55 +0100

    asasd

---
+++ Elysium.Domain/Database/UserRole.cs
@@ -0,0 +1,19 @@
+﻿using Elysium.Domain.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Elysium.Domain
+{
+    public class UserRole
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public Role Role { get; set; }
+    }
+}
